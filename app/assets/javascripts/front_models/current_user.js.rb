7359064e37dddd35b85ea8cf618f322a5aaf3f54
classyPimp <mussa88@mail.ru>
2015-10-30 16:15:20 +0600

    Current user updated to work with RequestHandler

--- app/assets/javascripts/front_models/current_user.js.rb
+++ app/assets/javascripts/front_models/current_user.js.rb
@@ -1,78 +1,52 @@
-=begin
-class Page < Model
-
-  attributes :id, :title, :body, :assoc
-  route "Find", get: "pages/:id"
-  route "create", post: "pages"
-  route "update", {put: "pages/:id"}, {defaults: [:id]}
-  route "destroy", {delete: "pages/:id"}, {defaults: [:id]}
-  
-end
-=end
-
 require "front_models/user"
 
 class CurrentUser < User
 
-  def self.route(name, method_and_url, options)
-    self.define_singleton_method(name.downcase) do | wilds = {}, req_options = {}|
-
-      url = prepare_http_url_from(method_and_url, wilds)
-
-      req_options = {payload: req_options}
-      
-      promise = Promise.new
-      HTTP.__send__(method_and_url.keys[0], url, req_options) do |response|
-        responses_on_get_current_user(response, promise) if name == "Get_current_user" 
-        responses_on_logout(response, promise) if name == "Logout"
-        responses_on_login(response, promise) if name == "Login"
-      end
-      promise
-    end
-  end
-
-  def self.responses_on_get_current_user(response, promise)
-    if response.ok?
-      unless response.json[:user][:id]
+  def self.responses_on_get_current_user(request_handler)
+    r = request_handler
+    if r.response.ok?
+      unless r.response.json[:user][:id]
         @logged_in = false
-        promise.reject("guest")
+        r.promise.reject("guest")
       else
-        x = Model.parse response.json
-        @user_instance = Model.parse(response.json)      
+        x = Model.parse r.response.json
+        @user_instance = Model.parse(r.response.json)      
         @logged_in = true
-        promise.resolve(@user_instance)
+        r.promise.resolve(@user_instance)
       end
     else
-      authorize!(response)
       @logged_in = false
       @user_instance = User.new
       "raise http error"
     end
   end
 
-  def self.responses_on_logout(response, promise)
-    if response.ok?
+  def self.responses_on_logout(request_handler)
+    r = request_handler
+    if r.response.ok?
       @logged_in = false
       @user_instance = User.new
-      promise.resolve(status: "ok") 
+      r.promise.resolve(status: "ok") 
     else
-      promise.reject(status: "error")
+      r.promise.reject(status: "error")
     end
   end
 
-  def self.responses_on_login(response, promise)
-    if response.ok?      
-      @user_instance = Model.parse(response.json)
+  def self.responses_on_login(request_handler)
+    r = request_handler
+    if r.response.ok?      
+      @user_instance = Model.parse(r.response.json)
       @logged_in = true
-      promise.resolve(status: "ok") 
+      r.promise.resolve(status: "ok") 
     else
-      promise.reject(statuts: "error")
+      r.promise.reject(statuts: "error")
     end
   end
 
   route "Get_current_user", post: "users/current_user"
   route "Logout", delete: "logout"
   route "Login", post: "login"
+  route "Request_password_reset"
 
   @user_instance ||= User.new
   @logged_in = false
