d54d6d1f203b707ac2bd5dbd85dec3d524da9d17
classyPimp <mussa88@mail.ru>
2015-11-10 17:16:32 +0600

    this commit is for pure example how rest actions can easily be done with validations and stuff

--- app/assets/javascripts/components/app_components/dummy.js.rb
+++ app/assets/javascripts/components/app_components/dummy.js.rb
@@ -1,3 +1,4 @@
+=begin
 class Dummy < RW
 
 	def initial_state
@@ -101,7 +102,7 @@ class MenuItem
   end
 
 end
-
+=end
 =begin
 class Page < Model
 	
@@ -128,7 +129,76 @@ class Page < Model
 
 end
 =end
-class Foo < RW
+class Page < Model
+
+  attributes :id, :body, :text, :user
+
+  route "create", post: "pages"
+  route "Index", get: "pages"
+  route "destroy", {delete: "pages/:id"}, {defaults: [:id]}
+  route "update", {put: "pages/:id"}, {defaults: [:id]}
+
+  def validate_body(options = {})
+    if body.length < 4
+      add_error(:body, "too short!")
+    end
+  end
+end
+
+
+class PageIndex < RW
+  expose
+
+  def initial_state
+    {
+      pages: ModelAssociation.new
+    }
+  end
+
+  def component_did_mount
+    Page.index.then do |pages|
+      set_state pages: pages
+    end
+  end
+
+  def render
+    t(:div,{},
+      *splat_each(state.pages) do |page|
+        t(:div, {key: "#{page}"},
+          t(:p, {}, "body: #{page.body}"),
+          t(:p, {}, "text: #{page.text}"),
+          t(:button, {onClick: ->(){init_page_edit(page)}}, "edit this page"),
+          t(:button, {onClick: ->(){destroy(page)}}, "destroy this page")
+          #t(:button, {onCLick: ->(){show(page)}})
+        )
+      end,
+      t(PageCreate, {on_create: ->(page){add_page(page)}}),
+      t(PageEdit, {on_edit_done: ->(page){update_page(page)}, ref: "page_edit_form"})
+    )
+  end
+
+  def destroy(page)
+    page.destroy.then do |r|
+      state.pages.remove(page)
+      set_state pages: state.pages
+    end
+  end
+
+  def init_page_edit(page)
+    ref(:page_edit_form).__opalInstance.init_page_edit(page)
+  end
+
+  def add_page(page)
+    (state.pages << page)
+    set_state pages: state.pages
+  end
+
+  def update_page(page)
+    set_state pages: state.pages
+  end
+end
+
+class PageCreate < RW
 	
   expose
 
@@ -136,73 +206,92 @@ class Foo < RW
 
 	def initial_state	
 		{
-			form_model: Model.parse(page: {email: "asd", password: "qwe", password_confirmation: "asd",
-                         pages: []})
+			form_model: false
 		}
 	end
 
+  def init_page_creation
+    self.set_state form_model: Page.new
+  end
+
 	def render
-=begin
-		t(:div, {},
-			input(Forms::Input, state.form_model, :email, {}),
-			input(Forms::Input, state.form_model, :password, {}),
-      input(Forms::Input, state.form_model, :password_confirmation, {reset_value: true}),
-      *splat_each_with_index(state.form_model.pages) do |model, index|
-        t(:div, {key: "#{model}"},
-          input(Forms::Input, model, :email, {}),
-          input(Forms::Input, model, :password, {}),
-          t(:button, {onClick: ->{remove(state.form_model.pages, index)}}, "remove")
-        ) unless model == nil
-      end,
-      t(:button, {onClick: ->{add(state.form_model.pages, Page.new)}}, "add"),
-      
-			t(:button, {onClick: ->{handle_inputs}}, "collect")
-		)
-=end
-	t(:p, {}, "The foo!")
+	  t(:div, {},
+      if state.form_model
+        t(:div, {}, 
+          input(Forms::Input, state.form_model, :text, {type: "text"}),
+          input(Forms::Input, state.form_model, :body, {type: "text"}),
+          t(:button, {onClick: ->(){handle_inputs}}, "create page")
+        )
+      else
+        t(:button, {onClick: ->(){init_page_creation}}, "I WANT SOME PAGES TO CREATE!")
+      end
+    )
 	end
 
   def handle_inputs
-    set_state form_model: collect_inputs
+    collect_inputs
+    unless state.form_model.has_errors?
+      state.form_model.create.then do |model|
+        if model.has_errors?
+          set_state form_model: model
+        else
+          set_state form_model: false
+          props.on_create(model)
+        end
+      end
+    else
+      set_state form_model: state.form_model
+    end
+  end
+end
+
+class PageEdit < RW
+  expose
+
+  include Plugins::Formable
+
+  def initial_state 
+    {
+      form_model: false
+    }
   end
 
-  def remove(_model, i)
-    _model.delete_at i
-    set_state form_model: state.form_model
+  def init_page_edit(page)
+    self.set_state form_model: page
   end
 
-  def add(_model, val)
-    _model.push val
-    set_state form_model: state.form_model
+  def render
+    t(:div, {},
+      if state.form_model
+        t(:div, {}, 
+          input(Forms::Input, state.form_model, :text, {type: "text"}),
+          input(Forms::Input, state.form_model, :body, {type: "text"}),
+          t(:button, {onClick: ->(){handle_inputs}}, "commit edit")
+        )
+      end
+    )
   end
 
+  def handle_inputs
+    collect_inputs
+    unless state.form_model.has_errors?
+      state.form_model.update.then do |model|
+        if model.has_errors?
+          set_state form_model: model
+        else
+          p "WHATTAP"
+          set_state form_model: false
+          props.on_edit_done(model)
+        end
+      end
+    else
+      set_state form_model: state.form_model
+    end.fail do |resp|
+      raise "PROMISE FAILED!"
+    end
+  end
 end
 
 
-class Page < Model
 
-	attributes :id, :body, :text, :user
-
-	route "create", post: "pages"
-
-end
-
-
-Document.ready? do
-	page = Page.new
-	page.text = "12345"
-	page.body = "The sexy body!"
-	page.create.then do |r|
-		page.validate
-		p "calling has_errors from response"
-		page.has_errors?
-		p "printin attributes"
-		p page.attributes
-		p "printing errors"
-		p page.errors
-		
-	end.fail do |r|
-		p r
-	end
-end
 
